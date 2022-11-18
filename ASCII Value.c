b9c18b16141c4a8178608cc604d00157bc683ddb
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-11-18 20:26:41 +0530

    Add files via upload

---
+++ ASCII Value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char s;
+    printf("Enter the character: ");
+    scanf("%c", &s);
+    printf("ASCII value of %c = %d",s,s);
+    return 0;
+}
